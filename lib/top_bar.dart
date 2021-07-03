08e808de71a718a261f654b78db419d0bd34f82c
neeraj2403 <jayarajneeraj@gmail.com>
2021-07-03 16:02:25 +0530

    edited7

--- lib/top_bar.dart
+++ lib/top_bar.dart
@@ -8,22 +8,35 @@ class TopBar extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(top: 35, left: 5),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          IconButton(
-            icon: Icon(
-              Icons.location_on,
-              color: Color.fromRGBO(58, 171, 58, 1),
-            ),
-            onPressed: () {},
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.location_on,
+                  color: Color.fromRGBO(58, 171, 58, 1),
+                ),
+                onPressed: () {},
+              ),
+              Text(
+                'Kochi, Kerala - 650601',
+                style: TextStyle(fontSize: 12, color: Colors.black),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_drop_down),
+              )
+            ],
           ),
-          Text(
-            'Kochi, Kerala - 650601',
-            style: TextStyle(fontSize: 12, color: Colors.black),
+          Padding(
+            padding: const EdgeInsets.only(right:25.0),
+            child: Container(
+                height: 20,
+                width: 20,
+                decoration: BoxDecoration(),
+                child: Image.asset('assets/Logo.png')),
           ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.arrow_drop_down),
-          )  
         ],
       ),
     );
